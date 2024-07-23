437e14982a72c7d5530e522e514e31efe0271d37
Vinicius V. Parizoto <vini.parizoto@live.com>
2024-07-23 15:13:03 -0300

    feat: integrando postgres com typeorm #feature/master

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CategoryModule } from './category/category.module'
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://postgres:Vin@0920@localhost:5432/devshop',
+      autoLoadEntities: true,
+      // Não é recomendado para versão em PROD -> utilizar migrations
+      synchronize: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
