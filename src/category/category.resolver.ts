0515af0a055df73d5eaa07d93b01f4938c7f633e
Vinicius V. Parizoto <vini.parizoto@live.com>
2024-07-24 17:55:41 -0300

    feat: endpoints de category #feature/master

--- src/category/category.resolver.ts
+++ src/category/category.resolver.ts
@@ -3,10 +3,19 @@ import { CategoryMapper } from './category.mapper'
 import { CategoryService } from './category.service'
 import { CategoryPublic } from './dto/category'
 import { CategoryCreateInput } from './dto/category-create.input'
+import { CategoryUpdateInput } from './dto/category-update.input'
 
 @Resolver(() => CategoryPublic)
 export class CategoryResolver {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Query(() => CategoryPublic, { name: 'getCategoryById' })
+  async getCategoryById(
+    @Args('id')
+    id: string,
+  ): Promise<CategoryPublic> {
+    return await this.categoryService.findById(id)
+  }
 
   @Query(() => [CategoryPublic], { name: 'getAllCategories' })
   async getAllCategories(): Promise<CategoryPublic[]> {
@@ -19,5 +28,21 @@ export class CategoryResolver {
     input: CategoryCreateInput,
   ): Promise<CategoryPublic> {
     return await this.categoryService.create(CategoryMapper.toEntity(input))
+  }
+
+  @Mutation(() => Boolean, { name: 'deleteCategory' })
+  async deleteCategory(
+    @Args('id')
+    input: string,
+  ): Promise<boolean> {
+    return await this.categoryService.delete(input)
+  }
+
+  @Mutation(() => CategoryPublic, { name: 'updateCategory' })
+  async updateCategory(
+    @Args('input')
+    input: CategoryUpdateInput,
+  ): Promise<CategoryPublic> {
+    return await this.categoryService.update(input)
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { Category } from './category.entity'
-import { CategoryPublic as CategoryDTO } from './dto/category'
+import { CategoryPublic } from './dto/category'
 
 @Injectable()
 export class CategoryService {
@@ -10,11 +10,29 @@ export class CategoryService {
     @InjectRepository(Category)
     private categoryRepository: Repository<Category>,
   ) {}
-  async findAll(): Promise<CategoryDTO[]> {
+  async findAll(): Promise<CategoryPublic[]> {
     return await this.categoryRepository.find()
+  }
+
+  async findById(id: string): Promise<CategoryPublic> {
+    return await this.categorFyRepository.findOne({ where: { id } })
   }
 
   async create(input: Category): Promise<Category> {
     return await this.categoryRepository.save(input)
+  }
+
+  async update(input: Category): Promise<Category> {
+    await this.categoryRepository.update(input?.id, input)
+    return input
+  }
+
+  async delete(id: string): Promise<boolean> {
+    try {
+      await this.categoryRepository.delete(id)
+      return true
+    } catch (err) {
+      return false
+    }
   }
 }
